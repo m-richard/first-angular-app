0698698c61b2baa2948c34700810faf1879a8e33
Marcus Richard <msrichard18@gmail.com>
2017-03-11 22:46:58 -0500

    got databinding component to render

--- src/app/databinding/databinding.component.ts
+++ src/app/databinding/databinding.component.ts
@@ -5,11 +5,7 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './databinding.component.html',
   styleUrls: ['./databinding.component.css']
 })
-export class DatabindingComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+export class DatabindingComponent {
+  stringInterpolation = 'This is string interpolation';
+  numberInterpolation = 3;
 }
