b1295312fbfbed05ea8a7ce869aae64ee01e14b3
Marcus Richard <msrichard18@gmail.com>
2017-03-12 21:39:36 -0400

    more property binding

--- src/app/databinding/databinding.component.ts
+++ src/app/databinding/databinding.component.ts
@@ -1,11 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { PropertyBindingComponent } from './property-binding.component';
 
 @Component({
   selector: 'fa-databinding',
   templateUrl: './databinding.component.html',
-  styleUrls: ['./databinding.component.css']
+  styleUrls: ['./databinding.component.css'],
 })
+
 export class DatabindingComponent {
   stringInterpolation = 'This is string interpolation';
   numberInterpolation = 3;
+
+  onTest(){
+    return true;
+  }
 }
